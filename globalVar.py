20648d10f97b623aa74a5d6e9a70ae864fbd0e54
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-13 13:43:30 +0100

    #globalVar modul u kojem se nalaze globalne promenljive i importi

---
+++ globalVar.py
@@ -0,0 +1,19 @@
+# globalne varijable i importi
+
+import copy
+import time
+import rangiranje
+from parrser import *
+from trie import *
+import glob
+import os
+from Skup import *
+from Graph import Graph
+import re
+
+global GRAPH
+GRAPH = Graph()
+global MAPA_TRIE
+MAPA_TRIE = {}
+RESULT_SET = Skup()
+RESULT_SKUP = []
