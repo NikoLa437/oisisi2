f0e6f4f23ce59d97ebe13a0994ce30d1a23c72a5
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-13 13:43:42 +0100

    #main

--- Main.py
+++ Main.py
@@ -1,20 +1,5 @@
-import copy
-import time
-import rangiranje
-from parrser import *
-from trie import *
-import glob
-import os
-from Skup import *
-from Graph import Graph
-import re
 
-global graph
-GRAPH = Graph()
-global MAPA_TRIE
-MAPA_TRIE = {}
-RESULT_SET = Skup()
-RESULT_SKUP = []
+from globalVar import *
 
 
 def ucitajPodatke(putanja):
