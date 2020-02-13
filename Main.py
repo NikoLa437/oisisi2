3dfbd0cc08b50bdd6db88043c1df0610e340b894
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-13 15:21:04 +0100

    #main

--- Main.py
+++ Main.py
@@ -19,13 +19,21 @@ def ucitajPodatke(putanja):
 
 
 def obicnaPretraga(kriterijum):
+    global RESULT_SET
+    RESULT_SET = Skup()
+    RESULT_SKUP= []
     for uslov in kriterijum:
         skup = Skup()
         for key in MAPA_TRIE:
-            bool, ponavaljanja = MAPA_TRIE[key].search(uslov)
+            bool, ponavaljanja = MAPA_TRIE[key].search(uslov.lower())
             if bool == "True":
-                skup.add(key)
-        RESULT_SKUP.append(skup)
+                skup.add(key) # dodaje u skup sve html dokumente koji sadrze datu rec
+        RESULT_SKUP.append(skup) # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
+    for file in RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
+        RESULT_SET = RESULT_SET | file
+
+def slozenijaPretraga(kriterijum, operacija):
+    print("Test: ", operacija)
 
 
 def rangirajSkup(niz_reci):
@@ -105,19 +113,30 @@ if __name__ == '__main__':
 
     putanja = input("Unesi putanju: ")
     ucitajPodatke(putanja)
-    kriterijum = input("Unesite kriterijum pretrage (reci odvojene razmakom + upotreba AND,OR,NOT), X za izlazak: ")
-    kriterijumArray = re.split(' ', kriterijum)
 
     while (True):
+        kriterijum = input("Unesite kriterijum pretrage (reci odvojene razmakom + upotreba AND,OR,NOT), X za izlazak: ")
+        kriterijumArray = re.split(' ', kriterijum)
         if kriterijum == "X":
             break
         else:
-            if "OR" not in kriterijumArray and "AND" not in kriterijumArray and "NOT" not in kriterijumArray:
-                obicnaPretraga(kriterijumArray)
-                RESULT_SET = Skup()
-                for file in RESULT_SKUP:
-                    RESULT_SET = RESULT_SET | file
-                rangirana_lista = rangirajSkup(kriterijumArray)
-                paginacijaRezultata(rangirana_lista)
-
-            break
+            print("Kriterijum pregrate ", kriterijumArray)
+            if "" in kriterijumArray:
+                print("Kriterijum je prazan! Pogresan unos")
+            else:
+                if "OR" not in kriterijumArray and "AND" not in kriterijumArray and "NOT" not in kriterijumArray:
+                    obicnaPretraga(kriterijumArray)
+                    rangirana_lista = rangirajSkup(kriterijumArray)
+                    paginacijaRezultata(rangirana_lista)
+                elif "OR" in kriterijumArray and "AND" not in kriterijumArray and "NOT" not in kriterijumArray:
+                    obicnaPretraga(kriterijumArray)
+                    rangirana_lista = rangirajSkup(kriterijumArray)
+                    paginacijaRezultata(rangirana_lista)
+                elif "OR" not in kriterijumArray and "AND" in kriterijumArray and "NOT" not in kriterijumArray:
+                    slozenijaPretraga(kriterijumArray, "AND")
+                    rangirana_lista = rangirajSkup(kriterijumArray)
+                    paginacijaRezultata(rangirana_lista)
+                elif "OR" not in kriterijumArray and "AND" not in kriterijumArray and "NOT" in kriterijumArray:
+                    slozenijaPretraga(kriterijumArray, "NOT")
+                    rangirana_lista = rangirajSkup(kriterijumArray)
+                    paginacijaRezultata(rangirana_lista)
