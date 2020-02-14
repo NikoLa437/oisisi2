688eb81e4d1990511b72de28307a6f9ee1c24444
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-14 03:22:45 +0100

    #globalVar update

--- globalVar.py
+++ globalVar.py
@@ -14,9 +14,10 @@ import sys
 
 global GRAPH
 GRAPH = Graph()
-global MAPA_TRIE
+global MAPA_TRIE # verovatno ce biti izbaceno
 MAPA_TRIE = {}
-global GLOBAL_TRIE
+global GLOBAL_TRIE # trie stablo koje sadrzi sve reci iz svih html dokumenata
 GLOBAL_TRIE= Trie()
-RESULT_SET = Skup()
-RESULT_SKUP = []
+RESULT_SET = Skup() # skup koji sadrzi html stranice koje ispunjavaju odredjeni uslov pretrage
+RESULT_SKUP = [] # globalna koja sluzi da cuva skup skupova koje treba porediti po AND OR NOT operatorima
+NADSKUP = Skup() # cuva skup svih stranica u fajlu (moze se koristiti za komplement)
