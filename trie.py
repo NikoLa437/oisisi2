1845ca8b98c517bd806b2618ea087ffae361aaff
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-14 03:26:14 +0100

    #Trie implementacija (dodato __bool__ i polje za stranice)

--- trie.py
+++ trie.py
@@ -7,8 +7,7 @@ class TrieNode():
         self.char = char  # karakter u cvoru
         self.children = {}  # pokazivac na decu u cvoru
         self.endOfWord = False  # oznaka kraja reci
-        self.counter = 0  # brojac pojavljivanja reci
-        self.stranice = Skup()
+        self.stranice = Skup() # polje koje ima Skup stranica koje sadrze odredjenu rec i cuva broj ponavljanja date reci (file,broj_ponavljanja)
 
 class Trie():
     def __init__(self):
@@ -21,20 +20,24 @@ class Trie():
             if char not in curr_node.children:
                 curr_node.children[char] = TrieNode(char)
             curr_node = curr_node.children[char]
-        curr_node.counter+=1
         curr_node.endOfWord = True
-        curr_node.stranice.add(file, 1)
+        curr_node.stranice.add(file, 1) # ako stavi true da se rec zavrsila nju unosi u skup stranica, ako vec postoji u skupu kljuc file
+                                        # onda ce u Skupu samo inkrementirati da se drugi put pojavila rec u datom fajlu!
 
     def search(self, word):
-        if word == "":
-            return "False",0, None
+
+        """if word == "": # proveriti
+            return "False",0, None"""
 
         curr_node = self.root
 
         for char in word:
             if char not in curr_node.children:
-                return "False",0,None
+                return "False",None
             curr_node = curr_node.children[char]
 
-        return str(curr_node.endOfWord),0, curr_node.stranice
+        return str(curr_node.endOfWord), curr_node.stranice
+
+    def __bool__(self): # vraca podatak da li postoji neki unos u Trie ili ne
+        return bool(self.root.char != "*" or self.root.children)
 
