6dd39faa800d0e3310b2e0378a452388580c9c77
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-14 03:24:10 +0100

    #pretraga_dokumenata implementirano za osnovnu upotrebu

--- pretrage.py
+++ pretrage.py
@@ -2,17 +2,56 @@ from globalVar import *
 
 def obicnaPretraga(kriterijum):
     global RESULT_SET
-    RESULT_SET= Skup()
+    RESULT_SET = Skup()
+    RESULT_SKUP= []
     for uslov in kriterijum:
-        skup = Skup()
-        for key in MAPA_TRIE:
-            bool, ponavaljanja = MAPA_TRIE[key].search(uslov.lower())
-            if bool == "True":
-                skup.add(key) # dodaje u skup sve html dokumente koji sadrze datu rec
-        RESULT_SKUP.append(skup) # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
+            bool, skup = GLOBAL_TRIE.search(uslov.lower())
+            if bool=="True":
+                RESULT_SKUP.append(skup) # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
     for file in RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
-        RESULT_SET = RESULT_SET | file
+        RESULT_SET = RESULT_SET | file # odradjuje se or metoda nad skupovima
+    for f in RESULT_SET:
+        print(f)
 
 
 def slozenijaPretraga(kriterijum, operacija):
-    print("Test: ", operacija)
+    global RESULT_SET
+    RESULT_SET = Skup()
+    RESULT_SKUP = []
+    if operacija=="AND":
+        for uslov in kriterijum:
+            bool, skup = GLOBAL_TRIE.search(uslov.lower())
+            if bool == "True":
+                RESULT_SKUP.append(skup)  # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
+                RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na AND operaciju!
+        for file in RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
+            RESULT_SET = RESULT_SET & file  # odradjuje se or metoda nad skupovima
+        for f in RESULT_SET:
+            print(f)
+    elif operacija=="NOT":
+        smestena_prva= False
+        for uslov in kriterijum:
+            bool, skup = GLOBAL_TRIE.search(uslov.lower())
+            if bool == "True":
+                RESULT_SKUP.append(skup)  # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
+                if not smestena_prva:
+                    RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na AND operaciju!
+                    smestena_prva=True
+                else:
+                    drugi_skup = skup
+
+        RESULT_SET = RESULT_SET - drugi_skup  # odradjuje se or metoda nad skupovima
+        for f in RESULT_SET:
+            print(f)
+    elif operacija=="KOMPLEMENT":
+        global NADSKUP
+        uslov= kriterijum[0]
+        bool,skup = GLOBAL_TRIE.search(uslov.lower())
+        if bool == "True":
+            RESULT_SET = skup  # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
+        RESULT_SET= RESULT_SET.komplement(NADSKUP)
+        for f in RESULT_SET:
+            print(f)
+
+
+
