773b792745a6d9eaa597c8a457c18b9c3f85d953
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-16 20:13:48 +0100

    #pretraga implementacija (optimizovana malo)

--- pretrage.py
+++ pretrage.py
@@ -1,29 +1,29 @@
 import globalVar
 from Skup import Skup
 
-
-def obicnaPretraga(kriterijum):
-    globalVar.RESULT_SET = Skup() ################### ZA DZONIJA - ovako pristupaj globalnim, u mainu izgleda ne mora, za GRAPH i GLOBAL_TRIE bude okej zato sto se tamo dodaje
-    RESULT_SKUP= []
-    for uslov in kriterijum:
-            bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())
-            if bool=="True":
-                RESULT_SKUP.append(skup) # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
-    for file in RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
-        globalVar.RESULT_SET = globalVar.RESULT_SET | file # odradjuje se or metoda nad skupovima
-
-
 def slozenijaPretraga(kriterijum, operacija):
     globalVar.RESULT_SET = Skup()
-    globalVar.RESULT_SKUP = []
-    if operacija=="AND":
+    smestena_prva = False
+    if operacija =="OR":
         for uslov in kriterijum:
-            bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())
-            if bool == "True":
-                globalVar.RESULT_SKUP.append(skup)  # niz skupova koji sadrze html dokumente koji ispunjavaju uslov
-                globalVar.RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na AND operaciju!
-        for file in globalVar.RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
-            globalVar.RESULT_SET = globalVar.RESULT_SET & file  # odradjuje se or metoda nad skupovima
+            bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())  # dobijamo skup pretrage
+            if bool == "True": # ako postoji skup
+                if not smestena_prva: # ako nismo smestili prvi
+                    globalVar.RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na || operaciju!
+                    smestena_prva=True
+                else:
+                    drugi_skup = skup # drugi skup za operaciju &
+        globalVar.RESULT_SET = globalVar.RESULT_SET | drugi_skup  # odradjuje se or metoda nad skupovima
+    elif operacija=="AND":
+        for uslov in kriterijum:
+            bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())  # dobijamo skup pretrage
+            if bool == "True": # ako postoji skup
+                if not smestena_prva: # ako nismo smestili prvi
+                    globalVar.RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na AND operaciju!
+                    smestena_prva = True
+                else:
+                    drugi_skup = skup # drugi skup za operaciju &
+        globalVar.RESULT_SET = globalVar.RESULT_SET & drugi_skup  # odradjuje se or metoda nad skupovima
     elif operacija=="NOT":
         smestena_prva= False
         for uslov in kriterijum:
@@ -35,7 +35,6 @@ def slozenijaPretraga(kriterijum, operacija):
                     smestena_prva=True
                 else:
                     drugi_skup = skup
-
         globalVar.RESULT_SET = globalVar.RESULT_SET - drugi_skup  # odradjuje se or metoda nad skupovima
     elif operacija=="KOMPLEMENT":
         uslov= kriterijum[0]
