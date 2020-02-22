f1a616c03b580e0af939ad7c53177f7fbb43366a
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-23 00:48:23 +0100

    #pretraga (dodato da tolerise python programming language)

--- Pretraga/pretrage.py
+++ Pretraga/pretrage.py
@@ -4,17 +4,15 @@ from StrukturePodataka.Skup import Skup
 def slozenijaPretraga(kriterijum, operacija):
     globalVar.RESULT_SET = Skup()
     smestena_prva = False
+    RESULT_SKUP = []
     drugi_skup = Skup()
     if operacija =="OR":
         for uslov in kriterijum:
             bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())  # dobijamo skup pretrage
             if bool == "True": # ako postoji skup
-                if not smestena_prva: # ako nismo smestili prvi
-                    globalVar.RESULT_SET= skup # postavljam bilo koji skup kao result set da posle ne bi isao prazan skup na || operaciju!
-                    smestena_prva=True
-                else:
-                    drugi_skup = skup # drugi skup za operaciju &
-        globalVar.RESULT_SET = globalVar.RESULT_SET | drugi_skup  # odradjuje se or metoda nad skupovima
+                RESULT_SKUP.append(skup) # ostale skupove dodajemo
+        for file in RESULT_SKUP:  # prolazimo kroz skup skupova html dokumenata koji ispunjavaju uslov i radimo logicko | za obicnu pretragu
+            globalVar.RESULT_SET = globalVar.RESULT_SET | file
     elif operacija=="AND":
         for uslov in kriterijum:
             bool, skup = globalVar.GLOBAL_TRIE.search(uslov.lower())  # dobijamo skup pretrage
