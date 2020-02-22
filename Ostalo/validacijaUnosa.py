03242f3f540b2c39b2eea96fec85375f3d8cc17f
Nikola Kolovic <nikoladskv@hotmail.rs>
2020-02-23 00:51:15 +0100

    #validacijaUnosa (provere za validnost unosa)

--- Ostalo/validacijaUnosa.py
+++ Ostalo/validacijaUnosa.py
@@ -1,35 +1,40 @@
 import re
 
 def validacijaUnosaObicnaPretraga(kriterijumArray):
-    if(len(kriterijumArray)>3 or "" in kriterijumArray):
-        print("POGRESAN UNOS")
-        print("Uneli ste vise od 2 kriterijuma pretrage")
+    if "" in kriterijumArray:
+        print("POGRESAN UNOS!")
+        print("Uneli ste prazan string u pretragu")
+        print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
         return False
     elif(len(kriterijumArray)==1 and (kriterijumArray[0]=="and" or kriterijumArray[0]=="or" or kriterijumArray[0]=="not")):
         print("POGRESAN UNOS")
-        print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)1")
+        print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)")
+        print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
         return False
-    elif(len(kriterijumArray)==2 and kriterijumArray[0] != "not" and (kriterijumArray[0]=="and" or kriterijumArray[0]=="or" or kriterijumArray[1]=="and" or kriterijumArray[1]=="or" or kriterijumArray[1]=="not")):
+    elif(len(kriterijumArray)==2 and (kriterijumArray[0]=="and" or kriterijumArray[0]=="or" or kriterijumArray[1]=="and" or kriterijumArray[1]=="or" or kriterijumArray[1]=="not")):
         print("POGRESAN UNOS")
-        print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)2")
-        return False
-    elif(len(kriterijumArray)==2 and kriterijumArray[0]== "not" and (kriterijumArray[1]=="and" or kriterijumArray[1]=="or" or kriterijumArray[1]=="not")):
-        print("POGRESAN UNOS")
-        print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)3")
+        print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)")
+        print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
         return False
     elif(len(kriterijumArray)==3):
         print(kriterijumArray)
         if(kriterijumArray[0]=="and" or kriterijumArray[0]=="or" or kriterijumArray[0]=="not"):
             print("POGRESAN UNOS")
-            print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)4")
+            print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)")
+            print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
             return False
         elif(kriterijumArray[2]=="and" or kriterijumArray[2]=="or" or kriterijumArray[2]=="not"):
             print("POGRESAN UNOS")
-            print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)5")
+            print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)")
+            print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
             return False
-        elif(kriterijumArray[1]!= "and" and kriterijumArray[1]!= "or" and kriterijumArray[1]!= "not"):
+        else:
+            return True
+    elif len(kriterijumArray)>3:
+        if "or" in kriterijumArray or "and" in kriterijumArray or "not" in kriterijumArray:
             print("POGRESAN UNOS")
-            print("Uneli ste u uslov pretrage neku od rezervisanih reci za operacije(or,and,not)6")
+            print("Uneli ste kriterijum pretrage sa vise od 2 uslova pri cemu ste koristili operator")
+            print("FORMAT: Uslov1 OPERATOR Uslov2 | NOT Uslov1 | Uslov1 Uslov2 Uslov3 ... UslovN")
             return False
         else:
             return True
@@ -40,7 +45,7 @@ def validacijaUnosaSlozenaPretraga(kriterijumArray):
     print(kriterijumArray)
     if kriterijumArray[0] == "||" or kriterijumArray[0] == "&&" or kriterijumArray[0]==")" or "" in kriterijumArray: # ne dozvoljava da krene sa && ili || ili )
         print("POGRESAN UNOS")
-        print("Prvi i poslednji string u naprednoj pretrazi ne moze biti && ili || ili )")
+        print("Prvi i poslednji string u naprednoj pretrazi ne moze biti && , || , ) ili imate prazan string kao kriterijum")
         return False
     elif kriterijumArray[len(kriterijumArray)-1] == "||" or kriterijumArray[len(kriterijumArray)-1] == "!" or kriterijumArray[len(kriterijumArray)-1] == "&&" or kriterijumArray[len(kriterijumArray)-1] == "(":
         print("POGRESAN UNOS") # ne dozvoljava da se pretraga zavrsava sa || && ili !
@@ -83,78 +88,4 @@ def validacijaUnosaSlozenaPretraga(kriterijumArray):
 
         return prolaz
 
-
-def parsirajNapredniUnos(kriterijum):
-    kriterijumArray = []
-    returnValAnd = []
-    returnVal = []
-
-    kriterijumArray = re.split(' ', kriterijum.lower())
-
-    for criteria in kriterijumArray:
-        if "||" in criteria and "||" != criteria:
-            podstring = criteria.split("||")
-            for criteriaOR in podstring:
-                if "&&" not in criteriaOR:
-                    returnValAnd.append(criteriaOR)
-                    if criteriaOR != podstring[len(podstring)-1]:
-                        returnValAnd.append("||")
-                else:
-                    podstring2= criteriaOR.split("&&")
-                    for criteriaAND in podstring2:
-                        returnValAnd.append(criteriaAND)
-                        if criteriaAND != podstring2[len(podstring2) - 1]:
-                            returnValAnd.append("&&")
-                        elif criteriaOR != podstring[len(podstring)-1]:
-                            returnValAnd.append("||")
-        elif "&&" in criteria and "&&" != criteria:
-            podstring = criteria.split("&&")
-            for criteriaOR in podstring:
-                if "||" not in criteriaOR:
-                    returnValAnd.append(criteriaOR)
-                    if criteriaOR != podstring[len(podstring) - 1]:
-                        returnValAnd.append("&&")
-                else:
-                    podstring2 = criteriaOR.split("||")
-                    for criteriaAND in podstring2:
-                        returnValAnd.append(criteriaAND)
-                        if criteriaAND != podstring2[len(podstring2) - 1]:
-                            returnValAnd.append("||")
-                        elif criteriaOR != podstring[len(podstring) - 1]:
-                            returnValAnd.append("&&")
-        else:
-            returnValAnd.append(criteria)
-
-    for criteria in returnValAnd:
-        if criteria[0]=="(" and criteria[len(criteria)-1] == ")":
-            returnVal.append("(")
-            returnVal.append(criteria[1:len(criteria)-2])
-            returnVal.append(")")
-        elif criteria[0]=="(" and criteria!="(":
-            if criteria[1] == "!":
-                returnVal.append("(")
-                returnVal.append("!")
-                returnVal.append(criteria[2:])
-            else:
-                returnVal.append("(")
-                returnVal.append(criteria[1:])
-        elif criteria[0]=="!" and criteria!="!":
-            if(criteria[1]=="("):
-                returnVal.append("!")
-                returnVal.append("(")
-                returnVal.append(criteria[2:])
-            elif(criteria[1]=="!"):
-                returnVal.append("!")
-                returnVal.append("!")
-                returnVal.append(criteria[2:])
-            else:
-                returnVal.append("!")
-                returnVal.append(criteria[1:])
-        elif criteria[len(criteria)-1] == ")" and criteria !=")":
-            returnVal.append(criteria[0:len(criteria)-1])
-            returnVal.append(")")
-        else:
-            returnVal.append(criteria)
-
-    return returnVal
 #( test && java ) && test || !(test||java)
